0eaba2bfc0ecbe30820758c7408688ae503ca44f
MahfujuRahmam <mahfujur15-14276@diu.edu.bd>
2026-02-06 16:18:36 +0600

    Add initial application structure with routes, tests, and configuration files
    - Created console route for inspiring quotes in routes/console.php
    - Added web route for the welcome view in routes/web.php
    - Configured .gitignore files for storage directories to exclude unnecessary files
    - Implemented basic feature and unit tests in tests/Feature and tests/Unit
    - Set up Vite configuration for asset management with Laravel and Tailwind CSS

---
+++ database/migrations/2026_02_06_101547_create_results_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('ashon_id')->constrained('ashons')->onDelete('cascade');
+            $table->foreignId('centar_id')->constrained('centars')->onDelete('cascade');
+            $table->foreignId('marka_id')->constrained('markas')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->double('total_vote');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('results');
+    }
+};
